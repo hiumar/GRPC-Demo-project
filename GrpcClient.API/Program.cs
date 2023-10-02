1d96c397505ba7159259a31ee990a1c66669250f
Umar Faraz <Umarfaraz005@gmail.com>
2023-10-02 23:00:45 +0500

    Add project files.

---
+++ GrpcClient.API/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcClient.API;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string serverUrl = "https://localhost:7193"; // Update with your server HTTPS address
+
+        using var channel = GrpcChannel.ForAddress(serverUrl);
+
+        var client = new PrimeNoService.PrimeNoServiceClient(channel);
+
+        while (true)
+        {
+            Console.Write("Enter a number (1-1000) to check if it's prime: ");
+            if (int.TryParse(Console.ReadLine(), out int number) && number >= 1 && number <= 1000)
+            {
+                var request = new PrimeNumber
+                {
+                    Id = 1,
+                    Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                    Number = number
+                };
+
+                try
+                {
+                    var response =  client.CheckPrime(request);
+
+                    Console.WriteLine($"Is Prime: {response.IsPrime}");
+                    Console.WriteLine($"Round Trip Time (RTT): {response.Rtt}ms");
+                }
+                catch (RpcException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Status}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number between 1 and 1000.");
+            }
+        }
+    }
+}
---
+++ GrpcServiceStreamer/Services/PrimeNumberService.cs
@@ -0,0 +1,55 @@
+﻿using Grpc.Core;
+using GrpcServiceStreamer.Protos;
+
+namespace GrpcServiceStreamer.Services
+{
+    public class PrimeNumberService: PrimeNoService.PrimeNoServiceBase
+    {
+       
+
+        private readonly ILogger<PrimeNumberService> _logger;
+        public PrimeNumberService(ILogger<PrimeNumberService> logger)
+        {
+            _logger = logger;
+        }
+        public override Task<PrimeNumberResponse> CheckPrime(PrimeNumber request, ServerCallContext context)
+        {
+            if (request != null && request.Id != 0 && request.Number != 0)
+            {
+                // Implement prime number verification logic here
+                bool isPrime = IsPrime(request.Number);
+
+                // Calculate RTT
+                long rtt = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - request.Timestamp;
+
+                _logger.LogInformation($"Received request: ID={request.Id}, Number={request.Number}, IsPrime={isPrime}, RTT={rtt}ms");
+
+                // Create and return response
+                return Task.FromResult(new PrimeNumberResponse
+                {
+                    IsPrime = isPrime,
+                    Rtt = rtt
+                });
+            }
+            else
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid headers"));
+            }
+        }
+
+
+        private bool IsPrime(long number)
+        {
+            if (number <= 1) return false;
+            if (number <= 3) return true;
+            if (number % 2 == 0 || number % 3 == 0) return false;
+
+            for (long i = 5; i * i <= number; i += 6)
+            {
+                if (number % i == 0 || number % (i + 2) == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
